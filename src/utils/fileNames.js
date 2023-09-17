355e8a91f41d22a662ef9d2bb6cc4087924d28d8
rustybiskop <rustybiskop@gmail.com>
2023-09-17 12:24:51 +0200

    precomputed file names

---
+++ src/utils/fileNames.js
@@ -0,0 +1,8 @@
+/**
+ * @description collection of precomputed file names
+ */
+
+export default [
+  "15-September-2023_searched_movie_links_tyron,jackassforever.jsonCipher.js",
+  "15-September-2023_trending_movie_links.jsonCipher.js",
+];

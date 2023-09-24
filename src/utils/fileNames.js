d03847d2c9569164052f13cb2eb9442c2455e17c
rustybiskop <144700732+rustybiskop@users.noreply.github.com>
2023-09-24 16:37:52 +0200

    Update fileNames.js

--- src/utils/fileNames.js
+++ src/utils/fileNames.js
@@ -3,7 +3,7 @@
  */
 
 export default [
+  "24-September-2023_trending_movie_links.jsonCipher.js",
   "15-September-2023_searched_movie_links_tyron,jackassforever.jsonCipher.js",
   "17-September-2023_searched_movie_links_oppenheimer,barbie,مشاهدةفيلمBlueBeetle2023مترجم,مشاهدةفيلمTilDeathDoUsPart2023مترجم.jsonCipher.js",
-  "24-September-2023_trending_movie_links.jsonCipher.js",
 ];

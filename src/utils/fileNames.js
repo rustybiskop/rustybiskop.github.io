704d7005bff2441b54f8019af56ab2bfde6a2f40
rustybiskop <144700732+rustybiskop@users.noreply.github.com>
2023-10-30 12:31:00 +0200

    Delete src directory

--- src/utils/fileNames.js
+++
@@ -1,9 +0,0 @@
-/**
- * @description collection of precomputed file names
- */
-
-export default [
-  "15-September-2023_searched_movie_links_tyron,jackassforever.jsonCipher.js",
-  "17-September-2023_searched_movie_links_oppenheimer,barbie,مشاهدةفيلمBlueBeetle2023مترجم,مشاهدةفيلمTilDeathDoUsPart2023مترجم.jsonCipher.js",
-  "1-October-2023_trending_movie_links.jsonCipher.js",
-];
